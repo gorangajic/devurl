5938b774ef8beec7da2abd7c03b556cd5a96cabf
Goran Gajic <fero.null@gmail.com>
2015-10-09 15:22:57 +0200

    eslint examples dir

--- examples/concurently/server.js
+++ examples/concurently/server.js
@@ -1,6 +1,8 @@
+'use strict';
+
 var http = require('http');
 var port = process.env.PORT || 3030;
-var url =
+
 http.createServer(function (request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'});
   response.end('Hello World\n');
--- examples/directly/server.js
+++ examples/directly/server.js
@@ -1,13 +1,16 @@
+"use strict";
+
 var http = require('http');
 var port = process.env.PORT || 3000;
-var url = 'http://localhost:' + port +'/';
-http.createServer(function (request, response) {
+var url = 'http://localhost:' + port + '/';
+
+http.createServer(function(request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'});
   response.end('Hello World\n');
 }).listen(port);
 
 
-if (process.env.NODE_ENV != "production") {
+if (process.env.NODE_ENV !== "production") {
   var devurl = require('..');
   devurl.add('dev.example.com', url);
   console.log('Server running at http://dev.example.com');
